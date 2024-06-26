019a27e170fdcebd3a98cf0ad0c0619dff9e4ea6
luis hernandez <lhern026@gmail.com>
2024-06-26 13:16:45 -0700

    wave addition:

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { WavyBackground } from "./WavyBackground"; // Adjust the import path as needed
+import { WavyBackground } from "./WavyBackground";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 const containerVariants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      duration: 0.5, // Faster transition duration
+      duration: 0.5,
       when: "beforeChildren",
-      staggerChildren: 0.1, // Faster staggering of child elements
+      staggerChildren: 0.1,
     },
   },
 };
 
 const itemVariants = {
   hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.3 } }, // Faster item transition
+  visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
 };
 
 const spanVariants = {
   hidden: { scale: 0 },
   visible: {
     scale: 1,
-    transition: { duration: 0.3, type: "spring", stiffness: 100 }, // Faster span transition
+    transition: { duration: 0.3, type: "spring", stiffness: 100 },
   },
 };
 
@@ -34,66 +35,61 @@ const About = () => {
       className="relative z-10"
       containerClassName="min-h-screen flex flex-col justify-center"
       waveWidth={50}
-      backgroundFill="#FF6B6B" // Use the primary color as the background
+      backgroundFill="#FF6B6B"
       blur={10}
       speed="fast"
       waveOpacity={0.5}
     >
-      <motion.div
-        className="relative z-10 container mx-auto px-6 py-12 text-center"
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-      >
+      <div className="flex flex-col justify-center items-center">
         <motion.div
-          className="flex flex-col items-center"
+          className="w-full p-4"
           variants={containerVariants}
+          initial="hidden"
+          animate="visible"
         >
-          <motion.img
-            src="https://static.wixstatic.com/media/5b4348_85d6bf0a809b41c899bac1a39c666dd2~mv2.jpg/v1/fill/w_1058,h_1168,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/5b4348_85d6bf0a809b41c899bac1a39c666dd2~mv2.jpg"
-            alt="Profile"
-            className="w-40 h-40 rounded-full border-4 border-white mb-6 shadow-lg"
-            whileHover={{ scale: 1.1 }}
-            variants={itemVariants}
-          />
-          <motion.h2
-            className="text-5xl font-bold mb-6 text-primary"
-            variants={itemVariants}
-            whileHover={{ scale: 1.05 }}
-          >
-            About Me
-          </motion.h2>
-          <motion.p
-            className="text-2xl text-background bg-primary bg-opacity-75 p-4 max-w-2xl leading-relaxed mb-8 rounded-md shadow-md"
-            variants={itemVariants}
-            whileHover={{ scale: 1.02 }}
-          >
-            Hello, my name is{" "}
-            <motion.span
-              className="text-accent font-semibold"
-              variants={spanVariants}
-            >
-              Hailey Cisneros
-            </motion.span>
-            . I am a multimedia visual artist from Riverside, California, and I
-            have always been passionate about creating art. I work digitally
-            using Procreate, and traditionally with watercolors, acrylics, and
-            pastels. Currently, I am a student at Cal State Fullerton, majoring
-            in Drawing and Painting. My future plans include becoming an art
-            teacher after graduating, opening an online shop, and doing
-            freelance work.
-          </motion.p>
-          <Link to="/projects">
-            <motion.div
-              className="bg-primary text-white py-3 px-8 rounded-full shadow-lg hover:bg-secondary transition duration-300 inline-block"
-              variants={itemVariants}
-              whileHover={{ scale: 1.1 }}
-            >
-              View My Work
-            </motion.div>
-          </Link>
+          <CardContainer className="inter-var">
+            <CardBody className="bg-white bg-opacity-20 backdrop-blur-md relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full max-w-4xl h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-3xl font-bold text-neutral-600 dark:text-white"
+              >
+                About Hailey Cisneros
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-xl max-w-xl mt-2 dark:text-neutral-300"
+              >
+                Hello, my name is Hailey Cisneros. I am a multimedia visual
+                artist from Riverside, California. I work digitally using
+                Procreate, and traditionally with watercolors, acrylics, and
+                pastels. Currently, I am a student at Cal State Fullerton,
+                majoring in Drawing and Painting. My future plans include
+                becoming an art teacher after graduating, opening an online
+                shop, and doing freelance work.
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <img
+                  src="https://static.wixstatic.com/media/5b4348_85d6bf0a809b41c899bac1a39c666dd2~mv2.jpg/v1/fill/w_1058,h_1168,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/5b4348_85d6bf0a809b41c899bac1a39c666dd2~mv2.jpg"
+                  className="w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt="Hailey Cisneros"
+                  style={{ height: "300px", objectFit: "contain" }}
+                />
+              </CardItem>
+              <div className="flex justify-center items-center mt-4">
+                <CardItem
+                  translateZ={20}
+                  as={Link}
+                  to="/projects"
+                  className="bg-primary text-white py-3 px-8 rounded-full shadow-lg hover:bg-secondary transition duration-300"
+                >
+                  View My Work
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
         </motion.div>
-      </motion.div>
+      </div>
     </WavyBackground>
   );
 };
